3e77fec4a46ecef5815deff3fd6a767d5c0093d3
wardenOfTheNorth <53744714+wardenOfTheNorth@users.noreply.github.com>
2019-08-08 08:14:16 +0530

    Create Problem30.java

---
+++ Problem30.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+class Box {
+    double len, wid, hei;
+
+    public Box(double len, double wid, double hei) {
+        this.len = len;
+        this.wid = wid;
+        this.hei = hei;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Box b = (Box) obj;
+        return len * hei * wid == b.len * b.hei * b.wid;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (len * hei * wid);
+    }
+
+    @Override
+    public String toString() {
+        return "Length = " + len + ", " + "Width = " + wid + ", " + "Height = " + hei + ", " + "Volume = " + (len * hei * wid);
+    }
+}
+
+public class Thirtieth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number of boxes");
+        int n = sc.nextInt();
+        Set<Box> boxes = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter box");
+            boxes.add(new Box(sc.nextDouble(), sc.nextDouble(), sc.nextDouble()));
+        }
+        System.out.println(boxes);
+    }
+}
